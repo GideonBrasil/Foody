ee7868a157e68511f044e8f51ee7b9c24839a308
GideonBrasil <gidxx_z@yahoo.com>
2019-04-01 10:07:59 -0600

    counter increment state

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -7,6 +7,26 @@ export default class Counter extends Component {
     // imageUrl: "https://picsum.photos/200"
   };
 
+  //   constructor() {
+  //     super();
+  //     this.handleIncrement = this.handleIncrement.bind(this);
+  //   }
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
   render() {
     return (
       <React.Fragment>
@@ -14,12 +34,14 @@ export default class Counter extends Component {
         <span style={{ fontSize: 20 }} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li key={tag}>{tag}</li>
-          ))}
-        </ul>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        {this.state.tags.length === 0 && "Please create a new tag!"}
+        {this.renderTags()}
       </React.Fragment>
     );
   }

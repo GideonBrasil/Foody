b3fd51bd35d995786b01e10ab380a56ef7cba49e
GideonBrasil <gidxx_z@yahoo.com>
2019-04-01 08:02:22 -0600

    styles through state and inline

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,13 +3,17 @@ import React, { Component } from "react";
 export default class Counter extends Component {
   state = {
     count: 0
+    // imageUrl: "https://picsum.photos/200"
   };
 
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        {/* <img src={this.state.imageUrl} alt="picsum" /> */}
+        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }

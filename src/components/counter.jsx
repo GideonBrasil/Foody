4b4592c3eef784c7ba44ac214868a3bc73dae0cb
GideonBrasil <gidxx_z@yahoo.com>
2019-04-01 08:11:29 -0600

    refactor badge classes into method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 
 export default class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
     // imageUrl: "https://picsum.photos/200"
   };
 
@@ -10,12 +11,18 @@ export default class Counter extends Component {
     return (
       <React.Fragment>
         {/* <img src={this.state.imageUrl} alt="picsum" /> */}
-        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">
+        <span style={{ fontSize: 20 }} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
